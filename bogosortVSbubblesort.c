14a749ed319e2faf8d169523de603755cc305bd8
Dawson Stroik <126594662+dawsonstroik@users.noreply.github.com>
2023-12-15 09:39:15 -0600

    Update bogosortVSbubblesort.c

--- bogosortVSbubblesort.c
+++ bogosortVSbubblesort.c
@@ -29,7 +29,7 @@ void bubbleSort(int array[], int top){//bubblesort algorithm that iterates throu
 		}
 	}
 }
-void randomizeArray(int array[], int top){//Randomizes array after fillArray function is called. Also is the same randomiser used in the bogosort algorithm. May not be totally random because bogosort orders the array much faster than it should.
+void randomizeArray(int array[], int top){//Uses Fischer Yates Shuffle to randomize array after fillArray function is called. Also is the same randomiser used in the bogosort algorithm. May not be totally random because bogosort orders the array much faster than it should. 
 	srand(time(NULL));
 	for(int i = top - 1; i > 0; i--){
 		int j = rand() % (i + 1);
